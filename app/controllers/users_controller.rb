9866951757b7eba1e0242df83431b42e65d633eb
purestmix <jfylucanas@gmail.com>
2016-04-10 17:01:31 +0000

    session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-   def show
+
+  def show
     @user = User.find(params[:id])
   end
+
   def new
-     @user = User.new
+    @user = User.new
   end
-    def create
+
+  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
